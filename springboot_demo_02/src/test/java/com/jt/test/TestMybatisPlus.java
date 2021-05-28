b6cc3ac483fbda29981819da7df229eb60260be4
Abo <252841200@qq.com>
2021-05-28 15:34:54 +0800

    1.添加日志打印配置 2.利用MP实现对象查询

--- springboot_demo_02/src/test/java/com/jt/test/TestMybatisPlus.java
+++ springboot_demo_02/src/test/java/com/jt/test/TestMybatisPlus.java
@@ -1,10 +1,13 @@
 package com.jt.test;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.UserMapper;
 import com.jt.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class TestMybatisPlus {
@@ -36,5 +39,33 @@ public class TestMybatisPlus {
         userMapper.insert(user);
     }
 
+    /**
+     * 查询id为23的数据
+     */
+    @Test
+    public void select01(){
+        User user = userMapper.selectById(23);
+        System.out.println(user);
+    }
+
+    /**
+     * 查询name=潘凤，sex=男 的数据
+     * 结果：一项:userMapper.selectOne();
+     *      多项:userMapper.selectList();
+     * SQL: ... where name="xxx" and sex="xxx"
+     * queryWrapper:条件构造器  用于拼接where条件
+     * 如果遇到多条件查询，则默认的连接符为and  如：SELECT id,name,age,sex FROM demo_user WHERE name=? AND sex=?
+     * 方式一：可以通过对象的方式进行控制
+     *
+     */
+    @Test
+    public void select02(){
+        User user = new User();
+        user.setName("潘凤").setSex("男");
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>(user);
+        //根据对象中不为null的属性拼接where条件
+        List<User> userList = userMapper.selectList(queryWrapper);
+        System.out.println(userList);
+    }
 
 }
