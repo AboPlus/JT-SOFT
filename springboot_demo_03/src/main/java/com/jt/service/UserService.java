ffa8d9e643ff7de72cd06b292bf2f4333890e6dc
Abo <252841200@qq.com>
2021-05-31 17:30:25 +0800

    ajax实现表单数据展现

--- springboot_demo_03/src/main/java/com/jt/service/UserService.java
+++ springboot_demo_03/src/main/java/com/jt/service/UserService.java
@@ -2,7 +2,11 @@ package com.jt.service;
 
 import com.jt.pojo.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void insertUser(User user);
+
+    List<User> getUser();
 }
--- springboot_demo_03/src/main/java/com/jt/service/UserServiceImpl.java
+++ springboot_demo_03/src/main/java/com/jt/service/UserServiceImpl.java
@@ -1,18 +1,29 @@
 package com.jt.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.UserMapper;
 import com.jt.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserMapper userMapper;
 
-
     @Override
     public void insertUser(User user) {
         userMapper.insert(user);
     }
+
+    @Override
+    public List<User> getUser() {
+        //因为是查询所有数据，即不需要where条件，故传入null
+        List<User> userList = userMapper.selectList(null);
+        return userList;
+    }
+
+
 }
