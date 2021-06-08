b2562dd2c2a7c6c5273e02d0ad06e8e0ebdc96f5
Abo <252841200@qq.com>
2021-06-08 21:59:21 +0800

    脚手架程序执行顺序

---
+++ jtadmin/src/router/index.js
@@ -0,0 +1,20 @@
+// import 导入vue.js  需要提前依赖
+import Vue from 'vue'
+// import 导入router.js
+import VueRouter from 'vue-router'
+// 手动导入自己的组件
+import Login from '../components/Login.vue'
+
+// 在vue对象中声明路由对象 整个脚手架中都可以引用路由
+Vue.use(VueRouter)
+const routes = [
+  {path: '/', redirect: '/login'},
+  {path: '/login', component: Login}
+]
+
+//创建路由对象
+const router = new VueRouter({
+  routes
+})
+
+export default router
