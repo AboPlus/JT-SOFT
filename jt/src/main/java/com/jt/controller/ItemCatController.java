8a192ad8ae4975f7972271099356876ff9efd409
Abo <252841200@qq.com>
2021-06-15 20:03:59 +0800

    商品分类查询

--- jt/src/main/java/com/jt/controller/ItemCatController.java
+++ jt/src/main/java/com/jt/controller/ItemCatController.java
@@ -1,11 +1,13 @@
 package com.jt.controller;
 
 import com.jt.mapper.ItemCatMapper;
+import com.jt.pojo.ItemCat;
 import com.jt.service.ItemCatService;
+import com.jt.vo.SysResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -13,4 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemCatController {
     @Autowired
     private ItemCatService itemCatService;
+
+    /**
+     *   业务需求：查询3级商品分类信息
+     * - 请求路径: /itemCat/findItemCatList
+     * - 请求类型: get
+     * - 请求参数: type (表示请求级别，等同于level字段)
+     * - 业务说明: 查询3级分类菜单数据 要求三层结构嵌套
+     * - 返回值: SysResult对象
+     */
+    @GetMapping("/findItemCatList/{type}")
+    public SysResult findItemCatList(@PathVariable Integer type){
+        // 要求实现数据的嵌套
+        List<ItemCat> catList = itemCatService.findItemCatList(type);
+        return SysResult.success(catList);
+    }
 }
--- jt/src/main/java/com/jt/service/ItemCatServiceImpl.java
+++ jt/src/main/java/com/jt/service/ItemCatServiceImpl.java
@@ -1,11 +1,40 @@
 package com.jt.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.ItemCatMapper;
+import com.jt.pojo.ItemCat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemCatServiceImpl implements ItemCatService{
     @Autowired
     private ItemCatMapper itemCatMapper;
+
+    /**
+     * type共有三种结果 ———— 1、2、3
+     */
+    Integer parentId = 0;
+    @Override
+    public List<ItemCat> findItemCatList(Integer type) {
+        // 递归实现
+        QueryWrapper<ItemCat> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("parent_id", parentId);
+        List<ItemCat> itemCats = itemCatMapper.selectList(queryWrapper);
+        for (ItemCat itemCat : itemCats){
+            if (itemCat.getLevel() == type) {
+                break;
+            }
+            parentId = itemCat.getId();
+            itemCat.setChildren(findItemCatList(type));
+        }
+        parentId = 0;
+        return itemCats;
+    }
 }
